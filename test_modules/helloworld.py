e6f1b32f708b3dba896be62dbbc8226f4717df6a
gbs <gbsalinetti@gmail.com>
2016-12-20 22:50:36 +0100

    update helloworld.py

--- test_modules/helloworld.py
+++ test_modules/helloworld.py
@@ -4,10 +4,11 @@
 # Tested with ansible/hacking/test-module utility
 
 from ansible.module_utils.basic import *
+import sys
 
 def main():
     module = AnsibleModule(argument_spec={})
-    response = {"hello": "world"}
+    response = {"hello": "world", "platform": sys.platform, "python_version": sys.version}
     module.exit_json(changed=False, meta=response)
 
 if __name__ == '__main__':
